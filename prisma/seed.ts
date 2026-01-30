3e520158d3b727c52b7f5c9cb9ce44f7bc12ff20
Sashankr <sashankr97@gmail.com>
2026-01-30 12:47:06 +0530

    using seeding script to seed categories

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,1 +1,48 @@
-console.log("Seeding database");
+import { PrismaClient, Prisma } from "../app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+export async function main() {
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+  const electronics = await prisma.category.create({
+    data: {
+      name: "Electoronics",
+      slug: "/electronics",
+    },
+  });
+
+  const clothing = await prisma.category.create({
+    data: {
+      name: "Clothing",
+      slug: "/clothing",
+    },
+  });
+
+  const home = await prisma.category.create({
+    data: {
+      name: "Home",
+      slug: "/home",
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    console.log("Seeding complete!");
+
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
