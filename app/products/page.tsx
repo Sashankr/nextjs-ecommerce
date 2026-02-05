fb87a3f112e8eba7163331d223dc9df5ce85b711
Sashankr <sashankr97@gmail.com>
2026-02-05 23:31:36 +0530

    product page update

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,7 +1,7 @@
 export default function Products() {
   return (
-    <div className="container">
-      <h1>Products</h1>
+    <div className="container mx-auto py-2">
+      <h1 className="text-lg text-black">Products</h1>
     </div>
   );
 }
